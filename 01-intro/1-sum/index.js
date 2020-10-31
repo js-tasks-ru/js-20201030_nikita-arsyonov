9a0cae3d7c20fcf2a08011e817317b6db855c2a7
Nikita Arsenov <orange.rs6@gmail.com>
2020-10-31 21:57:39 +0300

    first task

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,5 +5,13 @@
  * @returns {number}
  */
 export default function sum(m, n) {
+    let param_1 = m;
+    let param_2 = n;
 
+    // Если входящие параметры - не числа, считаю их нулевыми, и они не увеличат сумму.
+    // Например, при передаче в ф-ю (2,'три') - ожидаю на выходе число 2.
+    if( isNaN(param_1) ) { param_1 = 0; }
+    if( isNaN(param_2) ) { param_2 = 0; }
+
+    return param_1 + param_2;
 }
