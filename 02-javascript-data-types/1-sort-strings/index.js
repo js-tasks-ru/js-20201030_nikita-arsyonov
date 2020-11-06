7763e4c36d9ee971dc1fefdbab77b6e8785ae85f
Nikita Arsenov <orange.rs6@gmail.com>
2020-11-06 22:42:57 +0300

    Better late than never. Had a great experience, thanks!

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,21 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+/*
+Владимир, я своё решение не успел сдать, сомневался, и не успел доделать к занятию кроме этого ни одного задания.
+Сдаю в состоянии "как написал", тк снимать кальку с Вашего смысла не вижу никакого.
+PS Уяснил, что reverse() это плохой стиль и лишняя нагрузка на машину, не логично просто.
+ */
+
+export function sortStrings(arr, param = 'asc') {
+  let sortedArray = arr.slice().sort( (str1, str2) => str1.localeCompare( str2, ['ru', 'en'], {caseFirst: 'upper'}) );
+
+  if ( param === 'asc' ) {
+    return sortedArray;
+  } else if ( param === 'desc' ) {
+    return sortedArray.reverse();
+  } else {
+    return `ERROR! Input sort criteria [ ${param} ] is not 'asc' or 'desc'`;
+  }
 }
