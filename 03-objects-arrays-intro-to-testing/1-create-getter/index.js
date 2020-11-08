1f18d313779d11886f8c9b1f164bcfbe6ec3691c
Nikita Arsenov <orange.rs6@gmail.com>
2020-11-08 18:14:34 +0300

    1-st task solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const pathArray = path.split('.');
+  return function (data) {
+    for( let i in pathArray ) {
+      if ( data instanceof Object ) {
+        data = data[pathArray[i]];
+      }
+    }
+    return data;
+  };
 }
