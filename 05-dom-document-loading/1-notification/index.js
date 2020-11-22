c4db4f45a04f5f073ff5908e15e438f2c01242b5
Nikita Arsenov <orange.rs6@gmail.com>
2020-11-22 15:57:40 +0300

    task 1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
 export default class NotificationMessage {
+  // Статическое св-во, чтобы понять, есть ли уже NotificationMessage
+  // без инициализации класса ( без создания объекта ), а обратившись к NotificationMessage.activeNotification,
+  // где храним ссылку на элемент
 
+  static activeNotification;
+
+  constructor (
+    message = '', {
+    duration = 2000,
+    type =  'success'
+    } = {}) {
+
+    // Убрать из документа NotificationMessage, если он уже есть
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+
+    this.message = message;
+    this.duration = duration;
+    this.durationSeconds = duration / 1000; // перевести в секунды, иначе не идёт полоска!
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationSeconds}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">Notification</div>
+          <div class="notification-body">
+            ${this.message + Math.random()}
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+
+    // Сохранить ссылку на элемент
+    NotificationMessage.activeNotification = this.element;
+  }
+
+  show(parent = document.body) {
+    parent.append(this.element);
+
+    // Через указанный интервал - убрать сообщение
+    setTimeout( () => { this.remove(); }, this.duration);
+
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.activeNotification = null;
+  }
 }
