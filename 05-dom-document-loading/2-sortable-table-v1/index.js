4031690d9f0eb58340ad66e1d7b6499d9615bb02
Nikita Arsenov <orange.rs6@gmail.com>
2020-12-02 22:18:09 +0300

    2 tasks ready

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,154 @@
 export default class SortableTable {
+  element;
+  subElements = {};
 
+  constructor(headersConfig, {
+    data = []
+  } = {}) {
+    this.headersConfig = headersConfig;
+    this.data = data;
+
+    this.render();
+  }
+
+  // Отрисовать страницу
+  render() {
+    // Создать пустой <div> - обёртку
+    const wrapper = document.createElement('div');
+
+    // Получить HTML код таблицы
+    wrapper.innerHTML = this.getTable();
+
+    // Избавиться от обёртки
+    const element = wrapper.firstElementChild;
+
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+  }
+
+  // Получить HTML таблицы ( header+body )
+  getTable() {
+    return `
+      <div class="sortable-table">
+        ${this.getTableHeader()}
+        ${this.getTableBody()}
+      </div>`;
+  }
+
+  // Получить header
+  getTableHeader() {
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+      ${this.headersConfig.map(item => this.getHeaderRow(item)).join('')}
+    </div>`;
+  }
+
+  // Получить row и передать его для сборки с header
+  getHeaderRow({id, title, sortable}) {
+    return `
+      <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+        <span>${title}</span>
+        ${this.getHeaderSortingArrow()}
+      </div>
+    `;
+  }
+
+  // Стрелка направление сортировки
+  getHeaderSortingArrow() {
+    return `
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>`;
+  }
+
+  // Получить body
+  getTableBody() {
+    return `
+      <div data-element="body" class="sortable-table__body">
+        ${this.getTableRows(this.data)}
+      </div>`;
+  }
+
+  // Получить row передать его для сборки в body
+  getTableRows(data) {
+    return data.map(item => {
+      return `
+      <a href="/products/${item.id}" class="sortable-table__row">
+        ${this.getTableRow(item)}
+      </a>`;
+    }).join('');
+  }
+
+  getTableRow(item) {
+    const cells = this.headersConfig.map(({id, template}) => {
+      return {
+        id,
+        template
+      };
+    });
+
+    return cells.map(({id, template}) => {
+      return template
+        ? template(item[id])
+        : `<div class="sortable-table__cell">${item[id]}</div>`;
+    }).join('');
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+
+    const allColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+    const currentColumn = this.element.querySelector(`.sortable-table__cell[data-id="${field}"]`);
+
+    // TODO: Доработать в следующих заданиях
+    // Убрать стрелку сортировки с другой колонки
+    allColumns.forEach(column => {
+      column.dataset.order = '';
+    });
+
+    currentColumn.dataset.order = order;
+
+    this.subElements.body.innerHTML = this.getTableRows(sortedData);
+  }
+
+  sortData(field, order) {
+    const arr = [...this.data];
+    const column = this.headersConfig.find(item => item.id === field);
+    const {sortType, customSorting} = column;
+    const direction = {
+      'asc' : 1,
+      'desc': -1
+    };
+
+    return arr.sort((a, b) => {
+      switch (sortType) {
+      case 'number':
+        return direction[order] * (a[field] - b[field]);
+      case 'string':
+        return direction[order] * a[field].localeCompare(b[field], 'ru');
+      case 'custom':
+        return direction[order] * customSorting(a, b);
+      default:
+        return direction[order] * (a[field] - b[field]);
+      }
+    });
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.subElements = {};
+  }
 }
-
